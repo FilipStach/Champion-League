b483499b19d8588f923244134ec5b088ee5e0056
Filip Stach <filip.stach00@gmail.com>
2023-03-12 21:04:22 +0100

    First commit

---
+++ main.cpp
@@ -0,0 +1,26 @@
+//#include "mainwindow.h"
+
+//#include <QApplication>
+//using namespace std;
+
+//int main(int argc, char *argv[])
+//{
+//    QApplication a(argc, argv);
+//    MainWindow w;
+//    w.show();
+//    return a.exec();
+//    std::cout << "cccc";
+//}
+
+#include <iostream>
+using namespace std;
+
+// Driver Code
+int main()
+{
+    // Print standard output
+    // on the screen
+    cout << " byczku";
+
+    return 0;
+}
