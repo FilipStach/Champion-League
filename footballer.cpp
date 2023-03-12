bbfe3e158da7d61e4548d0dd068399eee21c747d
Filip Stach <filip.stach00@gmail.com>
2023-03-12 21:11:31 +0100

    Footballer implementation

---
+++ footballer.cpp
@@ -0,0 +1,6 @@
+#include "footballer.h"
+
+Footballer::Footballer()
+{
+
+}
---
+++ footballer.h
@@ -0,0 +1,11 @@
+#ifndef FOOTBALLER_H
+#define FOOTBALLER_H
+
+
+class Footballer
+{
+public:
+    Footballer();
+};
+
+#endif // FOOTBALLER_H
