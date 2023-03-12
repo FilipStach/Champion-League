ab339f35ebb8980181e8021cfd1a1628b0363699
Filip Stach <filip.stach00@gmail.com>
2023-03-12 21:27:04 +0100

    Footballer implementation

--- footballer.h
+++ footballer.h
@@ -1,11 +1,26 @@
 #ifndef FOOTBALLER_H
 #define FOOTBALLER_H
-
+#include <string>
+#include <vector>
+using namespace std;
 
 class Footballer
 {
+private:
+    int stamina;
+    int value;
+    vector <std::string> name;
+
 public:
     Footballer();
+    void decreaseStamina();
+    void increaseStamina();
+    int getStamina();
+    int getValue();
+    string toString();
+    string train();
+
+
 };
 
 #endif // FOOTBALLER_H
